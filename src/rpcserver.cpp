20ee14e1fcf34ab171bb4969120b2d3f9bab0ecd
yutotetuota <34832037+yutotetuota@users.noreply.github.com>
2018-11-04 11:17:24 +0900

    fix getwork method

--- src/rpcserver.cpp
+++ src/rpcserver.cpp
@@ -873,7 +873,7 @@ void JSONRequest::parse(const Value& valRequest)
     if (valMethod.type() != str_type)
         throw JSONRPCError(RPC_INVALID_REQUEST, "Method must be a string");
     strMethod = valMethod.get_str();
-    if (strMethod != "getblocktemplate")
+    if (strMethod != "getwork" && strMethod != "getblocktemplate")
         LogPrint("rpc", "ThreadRPCServer method=%s\n", SanitizeString(strMethod));
 
     // Parse params
